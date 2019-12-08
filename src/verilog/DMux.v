37d2322c45c748b0a645d0259c13e0a2b4681741
codecyang <dogco025@gmail.com>
2019-12-08 18:23:47 +0800

    Add demultiplexor

---
+++ src/verilog/DMux.v
@@ -0,0 +1,12 @@
+module DMux(in, sel, a, b);
+
+    input in, sel;
+    output a, b;
+    wire w;
+
+    Not not1(w1, sel);
+    And and1(a, in, w1);
+    And and2(b, in, sel);
+
+endmodule
+
---
+++ src/verilog/DMux_test.v
@@ -0,0 +1,22 @@
+module DMux_test;
+
+    reg in, sel;
+    wire a, b;
+
+    initial
+        begin
+            $dumpfile("DMux_test.vcd");
+            $dumpvars(0, dmux1);
+            $monitor("in = %b, sel = %b, a = %b, b = %b", in, sel, a, b);
+
+            #1 in = 1'b0; sel = 1'b0;
+            #1 in = 1'b0; sel = 1'b1;
+            #1 in = 1'b1; sel = 1'b0;
+            #1 in = 1'b1; sel = 1'b1;
+            #1 $finish;
+        end
+
+    DMux dmux1(in, sel, a, b);
+
+endmodule
+

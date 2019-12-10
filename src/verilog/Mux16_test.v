255b1679ca3a070c07df10e17faca52e88f9bd48
codecyang <dogco025@gmail.com>
2019-12-10 21:59:14 +0800

    Fix Mux16.

--- src/verilog/Mux16_test.v
+++ src/verilog/Mux16_test.v
@@ -1,19 +1,20 @@
 module Mux16_test;
 
-    reg[15:0] sel, a, b;
+    reg sel;
+    reg[15:0] a, b;
     wire[15:0] out;
 
     initial
         begin
             $dumpfile("Mux16_test.vcd");
             $dumpvars(0, mux16);
-            $monitor("sel = %h, a = %h, b = %h, out = %h", sel, a, b, out);
+            $monitor("sel = %b, a = %h, b = %h, out = %h", sel, a, b, out);
 
-            #1 sel = 16'h0; a = 16'h0; b = 16'hffff;
-            #1 sel = 16'h0f0f; a = 16'hffff; b = 16'h0000;
-            #1 sel = 16'hffff; a = 16'h1234; b = 16'h4321;
-            #1 sel = 16'h1234; a = 16'h1234; b = 16'h4321;
-            #1 sel = 16'h5555; a = 16'h1234; b = 16'h4321;
+            #1 sel = 1'b0; a = 16'h0; b = 16'hffff;
+            #1 sel = 1'b0; a = 16'hffff; b = 16'h0;
+            #1 sel = 1'b1; a = 16'hffff; b = 16'h0;
+            #1 sel = 1'b0; a = 16'h1234; b = 16'h4321;
+            #1 sel = 1'b1; a = 16'h1234; b = 16'h4321;
             #1 $finish;
         end
 

b947a3aee67d39153cec08b04b0714c809b885e6
codecyang <dogco025@gmail.com>
2019-12-19 20:52:39 +0800

    Improve half adder.

--- src/verilog/HalfAdder.v
+++ src/verilog/HalfAdder.v
@@ -2,9 +2,13 @@ module HalfAdder(sum, carry, a, b);
 
     output sum, carry;
     input a, b;
+    wire w0, w1, w2;
 
-    Xor g0(sum, a, b);
-    And g1(carry, a, b);
+    nand nand0(w0, a, b);
+    nand nand1(w1, a, w0);
+    nand nand2(w2, w0, b);
+    nand nand3(sum, w1, w2);
+    Not not0(carry, w0);
 
 endmodule
 

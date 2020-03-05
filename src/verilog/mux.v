b770f1064cccdb3c445c500d49df4d14d82758df
codecyang <dogco025@gmail.com>
2020-03-06 07:06:09 +0800

    Rewrite Mux4Way16.

--- src/verilog/mux.v
+++ src/verilog/mux.v
@@ -45,11 +45,11 @@ module Mux4Way16(out, sel, a, b, c, d);
     output[15:0] out;
     input[1:0] sel;
     input[15:0] a, b, c, d;
-    wire[15:0] out_ab, out_cd, in;
+    wire[15:0] out_ab, out_cd;
 
-    Mux16 mux16_0(out_ab, sel[0], a, b);
-    Mux16 mux16_1(out_cd, sel[0], c, d);
-    Mux16 mux16_2(out, sel[1], out_ab, out_cd);
+    Mux16 g0(out_ab, sel[0], a, b);
+    Mux16 g1(out_cd, sel[0], c, d);
+    Mux16 g2(out, sel[1], out_ab, out_cd);
 
 endmodule
 

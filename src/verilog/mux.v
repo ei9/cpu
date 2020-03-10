8782ab7b63721b2ae9578b5b26249b627fbab987
codecyang <dogco025@gmail.com>
2020-03-10 20:39:07 +0800

    Rewrite DMux4Way

--- src/verilog/mux.v
+++ src/verilog/mux.v
@@ -86,11 +86,11 @@ module DMux4Way(a, b, c, d, in, sel);
     output a ,b ,c, d;
     input in;
     input[1:0] sel;
-    wire w0, w1;
+    wire g0_a, g0_b;
 
-    DMux g0(w0, w1, in, sel[1]);
-    DMux g1(a, b, w0, sel[0]);
-    DMux g2(c, d, w1, sel[0]);
+    DMux g0(g0_a, g0_b, in, sel[1]);
+    DMux g1(a, b, g0_a, sel[0]);
+    DMux g2(c, d, g0_b, sel[0]);
 
 endmodule
 

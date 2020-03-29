9583e228e0fd3c5a7bf8980a8883dbe4659c42d4
codecyang <dogco025@gmail.com>
2020-03-29 18:17:54 +0800

    Rewrite FullAdder.

--- src/verilog/alu.v
+++ src/verilog/alu.v
@@ -19,13 +19,13 @@ module FullAdder(sum, carry, a, b, cin);
 
     output sum, carry;
     input a, b, cin;
-    wire g0_out, g2_out, g3_out;
+    wire axorb, g2_out, anandb;
 
-    Xor g0(g0_out, a, b);
-    Xor g1(sum, g0_out, cin);
-    nand g2(g2_out, g0_out, cin);
-    nand g3(g3_out, a, b);
-    nand g4(carry, g2_out, g3_out);
+    Xor  g0(axorb, a, b);
+    Xor  g1(sum, axorb, cin);
+    nand g2(g2_out, axorb, cin);
+    nand g3(anandb, a, b);
+    nand g4(carry, g2_out, anandb);
 
 endmodule
 

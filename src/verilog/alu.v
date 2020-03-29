e05b93eeaf4651b5d541ba6fdb873d8930c0f5f2
codecyang <dogco025@gmail.com>
2020-03-29 17:59:19 +0800

    Rewrite HalfAdder.

--- src/verilog/alu.v
+++ src/verilog/alu.v
@@ -6,11 +6,11 @@ module HalfAdder(sum, carry, a, b);
     input a, b;
     wire w0, w1, w2;
 
-    nand nand0(w0, a, b);
-    nand nand1(w1, a, w0);
-    nand nand2(w2, w0, b);
-    nand nand3(sum, w1, w2);
-    Not not0(carry, w0);
+    nand g0(w0, a, b);
+    nand g1(w1, a, w0);
+    nand g2(w2, w0, b);
+    nand g3(sum, w1, w2);
+    Not  g4(carry, w0);
 
 endmodule
 

768dfa105de16ace90251843d87fc46fd36e2e4c
codecyang <ei498@protonmail.com>
2020-05-25 21:57:23 +0800

    Edit Add2.asm.

--- src/assembly/Add2.asm
+++ src/assembly/Add2.asm
@@ -2,13 +2,13 @@
 // Computes: RAM[2] = RAM[0] + RAM[1]
 // Usage: put values in RAM[0], RAM[1]
 
-@0
+@R0
 D=M    // D = RAM[0]
 
-@1
+@R1
 D=D+M  // D = D + RAM[1]
 
-@2
+@R2
 M=D    // RAM[2] = D
 
 @6

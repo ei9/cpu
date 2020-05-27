7370b55085f774519d02f527508e09d2cd120c43
codecyang <ei498@protonmail.com>
2020-05-27 22:02:43 +0800

    Edit Mult.asm.

--- src/assembly/Mult.asm
+++ src/assembly/Mult.asm
@@ -4,29 +4,56 @@
 // File name: projects/04/Mult.asm
 
 // Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2],
+// respectively.)
 
 // Put your code here.
 
+// R2 = R0 * R1
+// ans = a * b
+//
+// a = R0;
+// b = R1;
+// ans = 0;
+// for(int i = 0 ; i < b ; i++){
+//     ans += a;
+// }
+//
+// -------------------------------------------------
+//
+//     i = 0
+//
+// LOOP:
+//     if i == R1 goto END
+//     R2 = R2 + R0
+//     i = i + 1
+//     goto LOOP
+//
+// END:
+
     @i
-    M=1
+    M=0    // i = 0
     @R2
-    M=0
+    M=0    // R2 = 0
+
 (LOOP)
     @i
-    D=M     // D=i
+    D=M    // D = i
     @R1
-    D=D-M   // i=i-R1
+    D=D-M  // D = i - RAM[1]
     @END
-    D;JGT   // If (i-R1) > 0 goto END
+    D;JEQ  // if (i - RAM[1]) == 0 goto END
+
     @R0
-    D=M     // D=R0
+    D=M    // D = RAM[0]
     @R2
-    M=D+M   // result=result+R0
+    M=M+D  // RAM[2] = RAM[2] + RAM[0]
     @i
-    M=M+1   // i++
+    M=M+1  // i += 1
+
     @LOOP
-    0;JMP   // Goto LOOP
+    0;JMP  // goto LOOP
+
 (END)
     @END
     0;JMP

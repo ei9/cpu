631ca6f1876e1ffa58c4cce97624df3790764e4f
codecyang <ei498@protonmail.com>
2020-06-14 19:53:51 +0800

    Rewrite Computer.v. Fix tb_Computer.v.

--- src/verilog/Computer.v
+++ src/verilog/Computer.v
@@ -8,15 +8,14 @@ module CPU(output writeM, output[15:0] outM, output[14:0] addressM,pc, input clk
     wire[15:0] Ain, Aout, AorM, ALUout, Dout, pcOut, addressMOut;
 
     // PC load.
-    or  g0(ngzr, ng, zr);  // ngzr = ng | zr
-    not g1(g, ngzr);       // g = out > 0 = !(ng|zr)
-    and g2(passLT, ng, I[2]);  // ngLT = (ng&LT) = out < 0
-    and g3(passEQ, zr, I[1]);  // zrEQ = (zr&EQ) = out = 0
-    and g4(passGT, g, I[0]);  // gGT = (g&GT) = out > 0
-    or  g5(passLE, passLT, passEQ);  // out <= 0
-    or  g6(pass, passLE, passGT);  // out <=> 0
+    wire g = ~(ng | zr);            // g = out > 0 = !(ng|zr)
+    wire passLT = ng & I[2];        // ngLT = (ng&LT) = out < 0
+    wire passEQ = zr & I[1];        // zrEQ = (zr&EQ) = out == 0
+    wire passGT = g & I[0];         // gGT = (g&GT) = out > 0
+    wire passLE = passLT | passEQ;  // out <= 0
+    wire pass = passLE | passGT;    // out <=> 0
 
-    and g7(PCload, I[15], pass);  // PCload = I15&J
+    wire PCload = I[15] & pass;     // PCload = I15&J
 
     // 16-bit ALU.
     assign AorM = I[12] ? inM : Aout;
@@ -27,29 +26,28 @@ module CPU(output writeM, output[15:0] outM, output[14:0] addressM,pc, input clk
     assign pc = pcOut[14:0];
 
     // Address register.
-    not g9(Atype, I[15]);  // A-instruction
-    and g10(AluToA, I[15], I[5]);  // AluToA = I[15] & d1
-    or  g11(Aload, Atype, AluToA);  // A-instruction or load to A-register
+    wire Atype = ~I[15];          // A-instruction
+    wire AluToA = I[15] & I[5];   // AluToA = I[15] & d1
+    wire Aload = Atype | AluToA;  // A-instruction or data load to A-register
 
     assign Ain = AluToA ? ALUout : I;
     Register A(Aout, clk, Aload, Ain);
 
     // Data register.
-    and g13(Dload, I[15], I[4]);  // Dload = I[15] & d2
+    wire Dload = I[15] & I[4];  // Dload = I[15] & d2
     Register D(Dout, clk, Dload, ALUout);
 
     // output
     assign addressM = Aout[14:0];
-    and g14(writeM, I[15], I[3]);  // writeM = I[15] & d3
-    assign outM = ALUout & ALUout;
+    wire writeM = I[15] & I[3];  // writeM = I[15] & d3
+    assign outM = ALUout;
 endmodule  // CPU.
 
 module Memory(output[15:0] out, input clk,load, input[15:0] in, input[14:0] address);
     wire[15:0] outM, outS, outK, outSK;
 
-    not g0(N14, address[14]);  // ram[13:0]
-    and g1(Mload, N14, load);  // Load ram.
-    and g2(Sload, address[14], load);  // Load screen.
+    wire Mload = (~address[14]) & load;  // Load ram (ram[13:0]).
+    wire Sload = address[14] & load;   // Load screen.
 
     RAM16K ram(outM, clk, Mload, address[13:0], in);
     RAM8K  screen(outS, clk, Sload, address[12:0], in);
--- src/verilog/tb_Computer.v
+++ src/verilog/tb_Computer.v
@@ -11,7 +11,7 @@ module tb_Computer;
         for(i = 0 ; i < 32 ; i = i + 1) begin
             $display("%4x: %x", i, c.rom.m[i]);
         end
-        $monitor("%4dns clk=%d pc=%d I=%d A=%d D=%d M=%d", $stime, clk, c.pc, c.I, c.addressM, c.cpu.Dout, c.outM);
+        $monitor("%4dns clk=%d pc=%d I=%d A=%d D=%d M=%d", $stime, clk, c.pc, c.I, c.addressM, c.cpu.Dout, c.inM);
         clk = 0;
         #10 reset = 1;
         #20 reset = 0;

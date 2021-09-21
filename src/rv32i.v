a45d7b6e496b4c9a4416c84a112b9e84a0713aad
ei9 <ei498@protonmail.com>
2021-09-21 22:01:13 +0800

    backup

--- src/rv32i.v
+++ src/rv32i.v
@@ -67,10 +67,12 @@ endmodule  // register file
 //    or     |  0001
 //    add    |  0010
 //    sub    |  0110
+//    xor    |  0111  TODO: This is temporary.
 `define AND 4'b0000
 `define OR  4'b0001
 `define ADD 4'b0010
 `define SUB 4'b0110
+`define XOR 4'b0111
 
 module alu(
     out,
@@ -99,6 +101,8 @@ module alu(
                 out = in_1 + in_2;
             `SUB:
                 out = in_1 - in_2;
+            `XOR:
+                out = in_1 ^ in_2;
             default:  // AND operation
                 out = in_1 & in_2;
         endcase
@@ -107,23 +111,65 @@ endmodule  // alu
 
 
 // control bus:
-// 10  reg_write
-// 9   alu select r2(1) or imm(0)
-// 8   mem read
-// 7   mem write
-// 6   branch
-// 5   mem or alu save to register
+// 9   reg_write
+// 8   alu select r2(1) or imm(0)
+// 7   mem read
+// 6   mem write
+// 5   branch (0: pc+4, 1: pc+offset)
+// 4   mem(1) or alu(0) save to register
 // 3~0 alu ctrl
+`define R_TYPE 7'h33
+`define S_TYPE 7'h23
+`define B_TYPE 7'h63
+`define J_TYPE 7'h67
 module ctrl_unit(
     out,
-    clk,
     ins
 );
 
-    output[10:0] out;
+    output[9:0] out;
     input clk;
     input[31:0] ins;
 
+    wire[6:0] opcode = ins[6:0];
+    wire[6:0] funt7 = ins[31:25];
+    wire[2:0] funt3 = ins[14:12];
+
+    reg[9:0] out;
+
+    always @(posedge clk or ins) begin
+        case(opcode)
+            `R_TYPE: begin
+                case(funt3)
+                    3'h0: begin
+                        if(~|funt7)
+                            out = 10'h302;  // add
+                        else
+                            out = 10'h306;  // sub
+                    end
+                    3'h1: begin
+                    end
+                    3'h2: begin
+                    end
+                    3'h3: begin
+                    end
+                    3'h4: begin
+                        out = 10'h307;  // xor. TODO: This is temporary.
+                    end
+                    3'h5: begin
+                    end
+                    3'h6: begin
+                        out = 10'h301;  // or
+                    end
+                    3'h7: begin
+                        out = 10'h300;  // and
+                    end
+                endcase
+            end
+            default: begin
+            end
+        endcase
+    end
 endmodule  // ctrl unit
 
 
@@ -136,16 +182,22 @@ module rv32i(
     reg[31:0] pc;  // program counter.
 
     wire[31:0] ins, r1, r2, r_data, alu_out, alu_in_2, imm, ram_out;
-    wire[10:0] ctrl;
+    wire[9:0] ctrl;
     wire[4:0] r_addr;
     wire zero;
 
     assign imm = 32'b0; // TODO: generate imm
     assign alu_in_2 = ctrl[9] ? r2 : imm;
+    assign r_data = ctrl[4] ? ram_out : alu_out;
+    assign alu_in_2 = ctrl[8] ? r2 : imm;
 
     rom       im(ins, pc);
-    reg_file  rf(r1, r2, clk, ctrl[10], r_addr, r_data, ins[19:15], ins[24:20]);
+    reg_file  rf(r1, r2, clk, ctrl[9], r_addr, r_data, ins[19:15], ins[24:20]);
     alu       a(alu_out, zero, ctrl[3:0], r1, alu_in_2);
-    ram       dm(ram_out, clk, ctrl[8], ctrl[7], alu_out, r2);
-    ctrl_unit cu(ctrl, clk, ins);
+    ram       dm(ram_out, clk, ctrl[7], ctrl[6], alu_out, r2);
+    ctrl_unit cu(ctrl, ins);
+
+    always @(posedge clk) begin
+        pc = (ctrl[5] & zero) ? pc + imm : pc + 4;
+    end
 endmodule  // rv32i
---
+++ src/tb_rv32i.v
@@ -0,0 +1,23 @@
+`include "rv32i.v"
+
+module tb_rv32i;
+    reg clk;
+    wire[31:0] out;
+    assign out = m.rf.regs[0];  // x1 as output
+
+    rv32i m(clk);
+
+    initial begin
+        $dumpfile("./tb_rv32i.vcd");
+        $dumpvars(0, m);
+        $monitor("%2d clk=%b ctrl=%h out=%h", $time, clk, m.ctrl, out);
+        $readmemb("program.dat", m.im.m);     // Load program to rom.
+        $readmemh("reg.dat", m.rf.regs);  // Load data to register file.
+        m.pc = 32'b0;
+        clk = 0;
+    end
+
+    always #1 clk = ~clk;
+
+    initial #10 $finish;
+endmodule  // tb_rv32i
